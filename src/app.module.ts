93816288a46cfba697c2c8eee0e0341d21db51d2
sanhdp <sanhdp@runsystem.net>
2024-08-01 16:52:31 +0700

    Coding

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectController } from './project/project.controller';
+import { ProjectService } from './project/project.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController, ProjectController],
+  providers: [AppService, ProjectService],
 })
 export class AppModule {}
