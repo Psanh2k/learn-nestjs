128e620393716ef2c5f41db654dec2811da2947c
sanhdp <sanhdp@runsystem.net>
2024-08-05 10:11:41 +0700

    Coding

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -15,8 +15,10 @@ export class UsersController {
     async getAllUsers(): Promise<ResponseData<UserEntity[]>> {
         try {
             const users = await this.usersService.findAllUsers();
+            this.logger.info(`User fetched: ${JSON.stringify(users)}`);
             return new ResponseData<UserEntity[]>(users, HttpStatus.SUCCESS, HttpMessage.SUCCESS);
         } catch (error) {
+            this.logger.error(`Error fetching users: ${error.message}`);
             return new ResponseData<UserEntity[]>(null, HttpStatus.ERROR, HttpMessage.ERROR);
         }
     }
@@ -25,7 +27,6 @@ export class UsersController {
     async getUserById(@Param('id', ParseIntPipe) id: number): Promise<ResponseData<UserEntity>> {
         try {
             const user = await this.usersService.findUserById(id);
-            this.logger.info(`User fetched: ${JSON.stringify(user)}`);
             return new ResponseData<UserEntity>(user, HttpStatus.SUCCESS, HttpMessage.SUCCESS);
         } catch (error) {
             this.logger.error(`Error fetching user: ${error.message}`);
