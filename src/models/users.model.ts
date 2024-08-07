bcce51f5cd00b2afb95bba7fbcd6df0ea6ecd421
sanhdp <sanhdp@runsystem.net>
2024-08-07 08:36:33 +0700

    Coding

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,13 +1,21 @@
 export class Users {
     id?: number;
-    categoryId?: number;
-    productName?: string;
-    price?: number;
+    name?: string;
+    email?: string;
+    password?: string;
+    google_id?: string;
+    is_admin?: number;
+    is_super?: number;
+    remember_token?: string;
 
-    constructor({ id, categoryId, productName, price }) {
+    constructor({ id, name, email, password, google_id, is_admin, is_super, remember_token }) {
         if (id != null) this.id = id;
-        if (categoryId != null) this.categoryId = categoryId;
-        if (productName != null) this.productName = productName;
-        if (price != null) this.price = price;
+        if (name != null) this.name = name;
+        if (email != null) this.email = email;
+        if (password != null) this.password = password;
+        if (google_id != null) this.google_id = google_id;
+        if (is_admin != null) this.is_admin = is_admin;
+        if (is_super != null) this.is_super = is_super;
+        if (remember_token != null) this.remember_token = remember_token;
     }
 }
